e302ff4369b26192abf42d0974cc97173efd4660
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-05-07 12:09:31 -0600

    Filtro y ordenación, coincidencia de patrones con LIKE, filtros globales

---
+++ Ejemplo01/Models/Attendance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ejemplo01.Models;
+
+[Table("Attendance")]
+public partial class Attendance
+{
+    [Key]
+    [Column("AttendanceID")]
+    public int AttendanceId { get; set; }
+
+    [Column("StudentID")]
+    public int? StudentId { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public bool Present { get; set; }
+
+    [ForeignKey("StudentId")]
+    [InverseProperty("Attendances")]
+    public virtual Student? Student { get; set; }
+}
--- Ejemplo01/Program.cs
+++ Ejemplo01/Program.cs
@@ -1,4 +1,5 @@
-﻿using Ejemplo01.Models;
+﻿
+using Ejemplo01.Data;
 
 using (var db = new SchoolContext())
 {
@@ -9,4 +10,64 @@ using (var db = new SchoolContext())
         Console.WriteLine($"Nombre = {student.Name}, " +
             $"Registrado = {student.Registered}");
     }
+
+    var attendance = db.Attendances.ToList();
+
+    foreach (var item in attendance)
+    {
+        Console.WriteLine("Fecha: {0}, Estudiante: {1}, Asistió: {2}",
+            item.Date, item.StudentId, item.Present);
+    }
+
+    // Filtro y ordenación
+    Console.WriteLine("Estudiantes registrados, " +
+        "ordenados alfabéticamente por su nombre");
+
+    var registeredStudents = db.Students
+                              .Where(s => s.Registered)
+                              .OrderBy(s => s.Name)
+                              .ToList();
+    foreach(var student in registeredStudents)
+    {
+        Console.WriteLine($"{student.Name}");
+    }
+
+    // Coincidencia de patrones con LIKE
+    Console.WriteLine("Estudiantes cuyo nombre comience con 'B'");
+
+    var studentsStartingWithB = db.Students
+                                 .Where(s => s.Name.StartsWith("B"))
+                                 .ToList();
+
+    foreach (var student in studentsStartingWithB)
+    {
+        Console.WriteLine($"{student.Name}");
+    }
+
+    // Filtro Globales
+    registeredStudents = db.GetRegisteredStudents().ToList();
+    Console.WriteLine("Estudiantes cuyo nombre comience con 'B'");
+    studentsStartingWithB = db.GetRegisteredStudents()
+                             .Where(s => s.Name.StartsWith("B"))
+                             .ToList();
+
+    foreach (var student in studentsStartingWithB)
+    {
+        Console.WriteLine($"{student.Name}");
+    }
+
+    Console.WriteLine("Estudiantes cuyo nombre contenga 'a' " +
+        "ordenados alfabéticamente por su nombre");
+    var studentWithAInName = db.GetRegisteredStudents()
+                              .Where(s => s.Name.Contains("a"))
+                              .OrderBy(s => s.Name)
+                              .ToList();
+    foreach (var student in studentWithAInName)
+    {
+        Console.WriteLine($"{student.Name}");
+    }
+
+    Console.WriteLine("Obtener los estudiantes que hayan asistido el día de hoy," +
+        "ordenados por su ID de asistencia (AttendanceID)");
+
 }
