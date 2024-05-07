9c295253cf30da8200472398cfe3c9e705a807c8
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-05-07 08:38:05 -0600

    Filtro y Ordenación, Coincidencia de Patrones con LIKE

--- Ejemplo01/Models/Student.cs
+++ Ejemplo01/Models/Student.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ejemplo01.Models;
 
 public partial class Student
 {
+    [Key]
+    [Column("StudentID")]
     public int StudentId { get; set; }
 
+    [StringLength(50)]
     public string Name { get; set; } = null!;
 
     public bool Registered { get; set; }
+
+    [InverseProperty("Student")]
+    public virtual ICollection<Attendance> Attendances { get; set; } = new List<Attendance>();
 }
--- Ejemplo01/Program.cs
+++ Ejemplo01/Program.cs
@@ -1,5 +1,4 @@
-﻿using Ejemplo01.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Ejemplo01.Data;
 
 using (var db = new SchoolContext())
 {
@@ -7,5 +6,38 @@ using (var db = new SchoolContext())
     foreach (var estudiante in estudiantes)
     {
         Console.WriteLine("Nombre: {0}", estudiante.Name);
+    }
+}
+
+
+using(var db = new SchoolContext())
+{
+    Console.WriteLine("Mostrar todos los registros de la tabla Attendance");
+    var asistencia = db.Attendances.ToList();
+    foreach(var attendance in asistencia)
+    {
+        Console.WriteLine("Fecha: {0}, Estudiante: {1}, Presente: {2}", attendance.Date, 
+            attendance.StudentId, attendance.Present);
+    }
+
+    // Filtro y Ordenación
+    Console.WriteLine("Estudiantes registrados, ordenados alfabéticamente por su nombre");
+    var registeredStudents = db.Students
+        .Where(s => s.Registered == true)
+        .OrderBy(s => s.Name)
+        .ToList();
+    foreach (var student in registeredStudents)
+    {
+        Console.WriteLine("Nombre: {0}", student.Name);
+    }
+
+    // Coincidencia de Patrones con LIKE
+    Console.WriteLine("Estudiantes cuyo nombre comience con 'B'");
+    var studentsStartingWithB = db.Students
+        .Where(s => s.Name.StartsWith("B"))
+        .ToList();
+    foreach (var student in studentsStartingWithB)
+    {
+        Console.WriteLine("Nombre: {0}", student.Name);
     }
 }
